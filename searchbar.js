a476e0d7b47bc63fb8d8dcbb7e2f8c54313d92b9
Daniel Wang <s92025592025@gmail.com>
2017-05-26 16:41:37 -0700

    able to search by hitting enter

--- searchbar.js
+++ searchbar.js
@@ -4,4 +4,10 @@
 	document.querySelector("#search-btn").onclick = function(){
 		window.location = "search.html?search=" + document.querySelector("#search-bar").value;
 	};
+
+	document.querySelector("#search-bar").onkeypress = function (event){
+		if(event.keycode == 13 || event.which == 13){
+			window.location = "search.html?search=" + document.querySelector("#search-bar").value;
+		}
+	};
 })();
